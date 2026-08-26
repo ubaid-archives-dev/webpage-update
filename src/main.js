c6e8fe02f7d2b7ef0dea4c0024e8ba6ecabc5b60
ubaid ur rehman <ubaid.archives@gmail.com>
2026-08-27 00:27:39 +0500

    add nasa api fetch logic to main.js

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+const url=`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+document.querySelector("#app").innerHTML = '<h1>loading NASA ASTRONOMY Picture..</h1>';
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#app").innerHTML = `
+            <h1>${data.title}</h1>
+            <p class="date">${data.date}</p>
+            <img src="${data.url}" alt="${data.title}" style="max-width: 100%; border-radius: 8px;" />
+        <p class="explanation">${data.explanation}</p>
+      </div>
+        `;
+    })
+    .cathc(err => {
+        console.error("Error fetching data:", err);
+        documetn.querySelector("#app").innerHTML  = `<p>Failed to load NASA data. Check console for details.</p>`;
+    })
